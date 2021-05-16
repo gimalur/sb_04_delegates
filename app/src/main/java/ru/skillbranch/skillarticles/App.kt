728658c23495763bc43a7810585014ec5d8fcf4a
Vitalii Protsenko (Mariupol) <v.protsenko@mobidev.biz>
2021-05-16 23:44:03 +0300

    testPrefs

--- app/src/main/java/ru/skillbranch/skillarticles/App.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/App.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.skillarticles
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import ru.skillbranch.skillarticles.data.PrefManager
 
@@ -21,12 +22,41 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        val nightMode = if (PrefManager().isDarkMode) {
+        val pref = PrefManager()
+        val nightMode = if (pref.isDarkMode) {
             AppCompatDelegate.MODE_NIGHT_YES
         } else {
             AppCompatDelegate.MODE_NIGHT_NO
         }
         AppCompatDelegate.setDefaultNightMode(nightMode)
+
+//        testPrefs(pref)
+    }
+
+    private fun testPrefs(pref: PrefManager) {
+        Log.e("App", "pref.testBoolean ${pref.testBoolean}")
+        pref.testBoolean = true
+        Log.e("App", "pref.testBoolean ${pref.testBoolean}")
+
+        Log.e("App", "pref.testDouble ${pref.testDouble}")
+        pref.testDouble = Double.MIN_VALUE
+        Log.e("App", "pref.testDouble ${pref.testDouble}")
+
+        Log.e("App", "pref.testString ${pref.testString}")
+        pref.testString = "test string value"
+        Log.e("App", "pref.testString ${pref.testString}")
+
+        Log.e("App", "pref.testFloat ${pref.testFloat}")
+        pref.testFloat = Float.MIN_VALUE
+        Log.e("App", "pref.testFloat ${pref.testFloat}")
+
+        Log.e("App", "pref.testInt ${pref.testInt}")
+        pref.testInt = Int.MIN_VALUE
+        Log.e("App", "pref.testInt ${pref.testInt}")
+
+        Log.e("App", "pref.testLong ${pref.testLong}")
+        pref.testLong = Long.MIN_VALUE
+        Log.e("App", "pref.testLong ${pref.testLong}")
     }
 
 }
