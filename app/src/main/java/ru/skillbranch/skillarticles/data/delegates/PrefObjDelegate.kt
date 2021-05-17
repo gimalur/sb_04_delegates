f406f3a495ca7e9ed2bebaf495754016422d23b6
Vitalii Protsenko (Mariupol) <v.protsenko@mobidev.biz>
2021-05-17 09:49:10 +0300

    PrefObjDelegate

--- app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefObjDelegate.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefObjDelegate.kt
@@ -1,17 +1,44 @@
 package ru.skillbranch.skillarticles.data.delegates
 
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import ru.skillbranch.skillarticles.data.PrefManager
 import ru.skillbranch.skillarticles.data.adapters.JsonAdapter
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-class PrefObjDelegate<T>(private val adapter: JsonAdapter<T>, private val customKey: String? = null) :
-    ReadWriteProperty<PrefManager, T?> {
-    override fun getValue(thisRef: PrefManager, property: KProperty<*>): T? {
-        TODO("Not yet implemented")
-    }
+class PrefObjDelegate<T>(
+    private val adapter: JsonAdapter<T>,
+    private val customKey: String? = null
+) : ReadWriteProperty<PrefManager, T?> {
+
+    private var _storedValue: T? = null
 
     override fun setValue(thisRef: PrefManager, property: KProperty<*>, value: T?) {
-        TODO("Not yet implemented")
+        _storedValue = value
+        thisRef.scope.launch {
+            thisRef.dataStore.edit { prefs ->
+                prefs[keyName(property)] = value.let(adapter::toJson)
+            }
+        }
     }
+
+    override fun getValue(thisRef: PrefManager, property: KProperty<*>): T? {
+        if (_storedValue == null) {
+            val flowValue = thisRef.dataStore.data
+                .map { prefs ->
+                    prefs[keyName(property)]?.let(adapter::fromJson)
+                }
+            _storedValue = runBlocking(Dispatchers.IO) { flowValue.first() }
+        }
+        return _storedValue
+    }
+
+    private fun keyName(property: KProperty<*>) = stringPreferencesKey(customKey ?: property.name)
+
 }
