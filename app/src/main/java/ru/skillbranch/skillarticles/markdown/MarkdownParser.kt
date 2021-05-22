31c5edef88ea5a89ba5b73d857a63977219e5536
Vitalii Protsenko (Mariupol) <v.protsenko@mobidev.biz>
2021-05-22 12:21:19 +0300

    05 - regexp groups

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
@@ -9,9 +9,17 @@ object MarkdownParser {
     private const val UNORDERED_LIST_ITEM_GROUP = "(^[*+-] .+$)"
     private const val HEADER_GROUP = "(^#{1,6} .+?$)"
     private const val QUOTE_GROUP = "(^> .+?$)"
+    private const val ITALIC_GROUP = "((?<!\\*)\\*[^*].*?[^*]?\\*(?!\\*)|(?<!_)_[^_].*?[^_]?_(?!_))"
+    private const val BOLD_GROUP = "((?<!\\*)\\*{2}[^*].*?[^*]?\\*{2}(?!\\*)|(?<!_)_{2}[^_].*?[^_]?_{2}(?!_))"
+    private const val STRIKE_GROUP = "((?<!~)~{2}[^~].*?[^~]?~{2}(?!~))"
+    private const val RULE_GROUP = "(^[-_*]{3}$)"
+    private const val INLINE_GROUP = "((?<!`)`[^`\\s].*?[^`\\s]`(?!`))"
+    private const val LINK_GROUP = "(\\[[^\\[\\]]*?]\\(.+?\\)|^\\[*?]\\(.*?\\))"
+    private const val ORDERED_LIST_ITEM_GROUP = "(^[\\d]*? .+$)"
 
     //result regex
-    private const val MARKDOWN_GROUPS = "$UNORDERED_LIST_ITEM_GROUP|$HEADER_GROUP|$QUOTE_GROUP"
+    private const val MARKDOWN_GROUPS = "$UNORDERED_LIST_ITEM_GROUP|$HEADER_GROUP|$QUOTE_GROUP" +
+            "|$ITALIC_GROUP|$BOLD_GROUP|$STRIKE_GROUP|$RULE_GROUP|$INLINE_GROUP|$LINK_GROUP"
 
     private val elementsPattern by lazy { Pattern.compile(MARKDOWN_GROUPS, Pattern.MULTILINE) }
     /**
@@ -27,7 +35,8 @@ object MarkdownParser {
      * clear markdown text to string without markdown characters
      */
     fun clear(string: String?): String? {
-        // TODO implement
+        // метод принимает строку содержащую markdown разметку и должен вернуть строку без markdown
+        // специфичных символов (понадобится в дальнейшем для реализации поиска по тексту)
         return null
     }
 
@@ -61,6 +70,8 @@ object MarkdownParser {
                 }
             }
 
+            // Группы внутри регулярных выражений имеют нумерацию от 0 и до количества групп.
+            // Где нулевая группа - это все выражение целиком (поэтому наш перебираемый range начинается с 1).
             when (group) {
                 // NOT FOUND -> BREAK
                 -1 -> break@loop
@@ -97,6 +108,63 @@ object MarkdownParser {
 
                     val subElements = findElements(text)
                     val element = Element.Quote(text, subElements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+                // ITALIC
+                4 -> {
+                    // text without "*{}*"
+                    text = string.subSequence(startIndex.inc(), endIndex.dec())
+
+                    val subElements = findElements(text)
+                    val element = Element.Italic(text, subElements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+                // BOLD
+                5 -> {
+                    // text without "**{}**"
+                    text = string.subSequence(startIndex.plus(2), endIndex.minus(2))
+
+                    val subElements = findElements(text)
+                    val element = Element.Bold(text, subElements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+                // STRIKE
+                6 -> {
+                    // text without "~~{}~~"
+                    text = string.subSequence(startIndex.plus(2), endIndex.minus(2))
+
+                    val subElements = findElements(text)
+                    val element = Element.Strike(text, subElements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+                // RULE
+                7 -> {
+                    // text without "***" insert empty character
+                    val element = Element.Rule()
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+                // INLINE CODE
+                8 -> {
+                    // text without "`{}`}`"
+                    text = string.subSequence(startIndex.inc(), endIndex.dec())
+
+                    val subElements = findElements(text)
+                    val element = Element.InlineCode(text, subElements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+                // LINK
+                9 -> {
+                    // full text for regex
+                    text = string.subSequence(startIndex, endIndex)
+
+                    val (title: String, link: String) = "\\[(.*)\\]\\((.*)\\)".toRegex().find(text)!!.destructured
+                    val element = Element.Link(link, title)
                     parents.add(element)
                     lastStartIndex = endIndex
                 }
@@ -176,6 +244,19 @@ sealed class Element {
         override val elements: List<Element> = emptyList()
     ) : Element()
 
+    /**
+    из markdown разметки:
+
+    before simple text `code` split `code with line break
+    not` work `only inline` after simple text
+
+    ```code block.code block.code block```
+    also it work for multiline code block
+    ```multiline code block
+    multiline code block
+    multiline code block
+    multiline code block```
+     */
     data class BlockCode(
         val type: Type = Type.MIDDLE,
         override val text: CharSequence,
@@ -183,5 +264,20 @@ sealed class Element {
     ) : Element() {
         enum class Type {START, END, MIDDLE, SINGLE }
     }
+
+    /**
+     из markdown разметки:
+
+    ![Philadelphia's Magic Gardens. This place was so cool!](/assets/images/philly-magic-gardens.jpg "Philadelphia's Magic Gardens")
+    ![Canada's Magic Land](/assets/images/canada-magic-land.jpg)
+    ![](/assets/images/empty.jpg "Empty Alt for Image")
+     */
+    data class Image(
+        val url: String,
+        val alt: String?,
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
 
 }
